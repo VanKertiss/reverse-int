f6d18721c41285f3e2311567952d8e9c7d03a9e3
Курнган <KurganV@ipd.org>
2023-08-01 17:33:25 +0200

    feat:my solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
-}
+    const arr = n.toString().split('').reverse();
+    const result = arr.filter(item => {
+        return item !== '-'
+    }).join('')
+    console.log(Number(result))
+    return Number(result);
+  }
